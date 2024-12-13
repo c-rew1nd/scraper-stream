f7591347e2662e2c86d10a61ac1694e60a47ac72
c-rew1nd <dreektseayger@gmail.com>
2024-12-13 21:17:36 +0530

    commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -27,7 +27,10 @@ import React from 'react'
 function DesktopSidebar() {
   return (
     <div className='hidden relative md:block min-w[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground boreder-r-2 border-separate'>
-      DesktopSidebar</div>
+      <div className='flex items-center justify-center gap-2 border-b-[1px] border-separate p-4'>
+        <logo />
+      </div>
+      </div>
   )
 }
 
