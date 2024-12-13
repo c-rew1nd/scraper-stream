2da6faa846afd437dd049c04aa4faab4fb06839e
c-rew1nd <dreektseayger@gmail.com>
2024-12-13 21:27:07 +0530

    commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import {cn} from '@/lib/utils'
 
-function Logo() {
+function Logo({
+    fontSize = "text-2xl",
+    iconSize = 20, 
+} : {
+    fontSize?: string;
+    iconSize?: number
+}) {
   return (
-    <div>Logo</div>
+    <Link href="/" className={cn(
+        "text-2xl font-extralight flex items-center gap-2", fontSize
+    )}></Link>
   )
 }
 
