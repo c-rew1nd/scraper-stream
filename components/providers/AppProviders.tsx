16fb6c48b7eac85ea3debf348e4ca3bc2e8dbcd7
c-rew1nd <dreektseayger@gmail.com>
2024-12-14 16:10:26 +0530

    bug fix #2

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "next-themes";
 
-export function AppProviders({ children }: { childern: React.ReactNode }) {
+export function AppProviders({ children }: { children: React.ReactNode }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
