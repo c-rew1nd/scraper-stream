c9ade6c2ddc5b4ff72aa2bfaed00c5eaff01f537
c-rew1nd <dreektseayger@gmail.com>
2024-12-14 13:28:34 +0530

    commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import {cn} from '@/lib/utils'
+import { SquareDashedMousePointer } from 'lucide-react';
 
 function Logo({
     fontSize = "text-2xl",
@@ -12,7 +13,16 @@ function Logo({
   return (
     <Link href="/" className={cn(
         "text-2xl font-extralight flex items-center gap-2", fontSize
-    )}></Link>
+    )}>
+      <div className='rounded-xl bg-gradient-to-r from-blue-500 to-blue-800 p-2'>
+        <SquareDashedMousePointer size={iconSize}
+        className='stroke-white' />
+      </div>
+      <div>
+        <span className='bg-gradient-to-r from-blue-700 to-blue-950 bg-clip-text text-transparent'>Scraper</span>
+        <span className='text-stone-700 dark:text-stone-300'>Stream</span>
+      </div>
+    </Link>
   )
 }
 
