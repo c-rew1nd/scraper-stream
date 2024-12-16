d15d1a400890cbb2ff320c635d223f9c26b2308c
c-rew1nd <dreektseayger@gmail.com>
2024-12-16 10:18:08 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProviders } from "@/components/providers/AppProviders";
+import {ClerkProvider} from "@clerk/nextjs"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider afterSignOutUrl={"/sign-in"}>
     <html lang="en">
       <body className={inter.className}>
         <AppProviders>
@@ -23,5 +26,6 @@ export default function RootLayout({
         </AppProviders>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
